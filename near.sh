1c0aaf2d555eea77b8dc13341e5815f859f07a4f
MinhChain <81641466+securitycoder@users.noreply.github.com>
2024-05-01 16:23:35 +0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-0d34a5a4-3aeb-424a-a936-63716b96a7c4/"
 
 OUTPUT_DIR="logs"
 
